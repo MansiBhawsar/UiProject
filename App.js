dd8471376787d8555a625b35fb1acc41742e457e
MansiBhawsar <mansi.bhawsar1297@gmail.com>
2021-02-10 20:06:51 +0530

    prodata

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React,{useEffect,useState} from 'react';
 import { StyleSheet, Text, View,Image,SafeAreaView,ScrollView} from 'react-native';
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards';
+
 import axios from 'axios';
 
 export default function App() {
   const [posts,setposts]=useState([])
+
   useEffect(()=>{
     axios.post("https://testexpapp.expertrons.com/api/jobs/app/get-jobs",{
    textSearch:"",
@@ -25,19 +27,40 @@ export default function App() {
       <ScrollView>
      
     {
-    posts.map((p)=>{
+  posts.map((p)=>{
       // console.log(p);
     return(
   <Card>
+    
+  <View style={{flexDirection:'row', flexWrap:'wrap'}}>
   <CardImage 
-    source={{uri: 'p.companyId.image'}} 
-    style = {{ width: 100, height: 100 }}
+
+  source={{uri:"https://expertrons-v2.s3.ap-south-1.amazonaws.com/"+p.companyId.image}} 
+  style={{
+    
+    position: 'absolute',
+    marginLeft:20,
+    marginTop:20,
+    width:'20%',
+    height:'40%'
+   
+    
+   
+   }}
+
   />
   <CardTitle 
     title={p.title} 
+    style={{marginLeft:100}}
     subtitle="This is subtitle"
    />
-  <CardContent text="Mumbai, Pune, Ranchi, Delhi."/>
+   </View>
+   
+  <CardContent
+
+  text={'Mumbai, Pune, Ranchi, Delhi'}
+  style={{marginLeft:100}}
+  />
   </Card>
  );
 })
@@ -46,12 +69,15 @@ export default function App() {
 </SafeAreaView>
 );
 }
-
 const styles = StyleSheet.create({
   container: {
     width:"100%",
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: '#fff',
+    height: "100%",
+    left: "0%",
+    top: "0%",
+    display:"flex"
     
   },
 });
