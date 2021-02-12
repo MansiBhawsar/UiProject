c52f68584778c0d90cfe3d4fa0835c6a8fafda16
MansiBhawsar <mansi.bhawsar1297@gmail.com>
2021-02-12 20:03:44 +0530

    UI

--- App.js
+++ App.js
@@ -1,83 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { SearchBar } from 'react-native-elements';
 import React,{useEffect,useState} from 'react';
-import { StyleSheet, Text, View,Image,SafeAreaView,ScrollView} from 'react-native';
+import { StyleSheet, Text, View,Image,SafeAreaView,ScrollView,Button,Footer,FooterTab,Icon} from 'react-native';
 import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards';
-
 import axios from 'axios';
+import Jobs from './Jobs';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 
+const Tab = createBottomTabNavigator();
+function SettingsScreen(){
+  return(
+    <View>
+      <Text>Settingscreen</Text>
+    </View>
+  )
+}
+function ExploreScreen(){
+  return(
+    <View>
+      <Text>ExploreScreen</Text>
+    </View>
+  )
+}
 export default function App() {
-  const [posts,setposts]=useState([])
-
-  useEffect(()=>{
-    axios.post("https://testexpapp.expertrons.com/api/jobs/app/get-jobs",{
-   textSearch:"",
-    page:1
-  },{
-    headers:{
-      authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOGQ4N2FhOThiZDI5NTk0ODhjNmU5NiIsInVzZXJUeXBlIjoxLCJpYXQiOjE1ODYzMzM2MjMsImV4cCI6MTYxNzg2OTYyM30.J0n8wl8YBHE3emWl6VkIouPaZQHAiua_HREETZSiKBo"
-    }
-  }).then((res)=>{
-    // console.log(JSON.stringify(res.data.data.data));
-    setposts(res.data.data.data)
-  })
-  },[posts])
-  
   return (
-    <SafeAreaView style={styles.container}>
-      <ScrollView>
-     
-    {
-  posts.map((p)=>{
-      // console.log(p);
-    return(
-  <Card>
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Jobs} />
+      <Tab.Screen name="Search" component={Jobs} />
+      <Tab.Screen name="Course" component={SettingsScreen} />
+      <Tab.Screen name ="Profile" component={ExploreScreen}/>
+   </Tab.Navigator>
+   </NavigationContainer>
     
-  <View style={{flexDirection:'row', flexWrap:'wrap'}}>
-  <CardImage 
-
-  source={{uri:"https://expertrons-v2.s3.ap-south-1.amazonaws.com/"+p.companyId.image}} 
-  style={{
-    
-    position: 'absolute',
-    marginLeft:20,
-    marginTop:20,
-    width:'20%',
-    height:'40%'
-   
-    
-   
-   }}
-
-  />
-  <CardTitle 
-    title={p.title} 
-    style={{marginLeft:100}}
-    subtitle="This is subtitle"
-   />
-   </View>
-   
-  <CardContent
-
-  text={'Mumbai, Pune, Ranchi, Delhi'}
-  style={{marginLeft:100}}
-  />
-  </Card>
- );
-})
+  );
 }
-</ScrollView>
-</SafeAreaView>
-);
-}
-const styles = StyleSheet.create({
-  container: {
-    width:"100%",
-    flex: 1,
-    backgroundColor: '#fff',
-    height: "100%",
-    left: "0%",
-    top: "0%",
-    display:"flex"
-    
-  },
-});
+
+
+
+
---
+++ Jobs.js
@@ -0,0 +1,128 @@
+
+import { SearchBar } from 'react-native-elements';
+import React,{useEffect,useState} from 'react';
+import { StyleSheet, Text, View,Image,SafeAreaView,ScrollView,Button,Footer,FooterTab,Icon} from 'react-native';
+import { Card, CardTitle, CardContent, CardAction, CardButton, CardImage } from 'react-native-material-cards';
+import axios from 'axios';
+
+export default function Jobs() {
+
+  const [posts,setposts]=useState([])
+  const [search,setsearch]=useState("")
+ 
+
+ useEffect(()=>{
+  callsearchapi()
+  },[posts])
+
+  const callsearchapi =()=>{
+    axios.post("https://testexpapp.expertrons.com/api/jobs/app/get-jobs",{
+      textSearch:search,
+       page:1
+     },{
+       headers:{
+         authorization:"Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOGQ4N2FhOThiZDI5NTk0ODhjNmU5NiIsInVzZXJUeXBlIjoxLCJpYXQiOjE1ODYzMzM2MjMsImV4cCI6MTYxNzg2OTYyM30.J0n8wl8YBHE3emWl6VkIouPaZQHAiua_HREETZSiKBo"
+       }
+     }).then((res)=>{
+       // console.log(JSON.stringify(res.data.data.data));
+       setposts(res.data.data.data)
+     })
+  }
+
+  const updateSearch = (v) => {
+      setsearch(v);
+      callsearchapi();
+      };
+    
+    
+  return (
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <SearchBar 
+      style={{
+      backgroundColor:"white",
+      }}
+      placeholder="Find Jobs here....."
+      value={search}
+      onChangeText={updateSearch}
+      />
+     {
+       
+  posts.map((p,i)=>{
+      // console.log(p);
+
+  return(
+  <Card  key={i}> 
+  <View style={{
+    flexDirection:'row', 
+    flexWrap:'wrap',
+    singleLineTitle:"true"
+   }}>
+  <CardImage 
+  singleLineTitle="true"
+  source={{uri:"https://expertrons-v2.s3.ap-south-1.amazonaws.com/"+p.companyId.image}} 
+  style={styles.setBorder}
+     />
+  <CardTitle 
+    title={p.title} 
+    subtitle="This is subtitle"
+    titleStyle={styles.setFontSizeOne}
+    subtitleStyle={styles.subtitle}
+    
+   />
+   </View>
+   
+  <CardContent  
+ textStyle={styles.setContent}
+  text={p.location.map((l)=>{
+    return l.cityId.name
+  }).join(",")}
+ 
+  />
+  </Card>
+ );
+})
+}
+</ScrollView>
+</SafeAreaView>
+);
+}
+
+const styles = StyleSheet.create({
+  container: {
+  marginTop:3,
+  },
+
+  setBorder:{
+    borderRadius: 60,
+    position: 'absolute',
+    marginLeft:20,
+    marginTop:28,
+    width:"15%",
+    height:"35%",
+    borderRadius:50,
+    borderColor: 'black'
+  },
+
+  setFontSizeOne:{
+    marginLeft:80,
+    marginTop:10,
+    fontSize:16,
+    fontWeight:"bold"
+  },
+  subtitle:{
+    marginLeft:80,
+    fontSize:12,
+    fontWeight: "bold"
+
+  },
+
+  setContent:{
+    marginLeft:82,
+    fontSize:10,
+    fontWeight:"300"
+    
+  
+  }
+});
+
